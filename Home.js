58de34a430d18d4aad369da85a60351af7ba44e0
Jen990077 <jenleverenz@yahoo.com.sg>
2015-10-20 19:48:31 -0400

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,58 +1,26 @@
-$(document).ready(function ()
+
+function MouseOverListItem(li)
 {
-    var cells = $(".cell");
-    var colorCount = 0;
-
-    for (var i = 0; i < cells.length; i++)
-    {
-        var cell = $(cells[i]);
-        var isDark = colorCount % 2 == 0;
-        var isNextRow = (i + 1) % 8 == 0;
-        colorCount += isNextRow ? 2 : 1;
-        cell.css("background-color", isDark ? "navy" : "white");
-    }
-});
-
-var Home = {};
-var empty ={}; 
-var highlight={};
-var nextCell={}; 
-var sameCell={};
-
-
-Home.WireClickEvent = function () {
-
-    $(".cell").click(function () {
-    
-    if ()
-    {
-        $(this)
-
-    else if(){
-        $(this)
-            }
-    else if(){
-        $(this)
-        
-    }
-    else()
-    {
-        $(this)
-    }
-    });
+	li.className = "list-item-highlight";
 }
 
-
-
-Home.WireDragEvent = function () {
-    $(".piece").draggable()
-    $(this).css("background-color", "yellow");
+function MouseOutListItem(li)
+{
+	li.className = "";
 }
 
+Date.prototype.addDays = function(days)
+{
+    var dat = new Date(this.valueOf());
+    dat.setDate(dat.getDate() + days);
+    return dat;
+}
+
+var dat = new Date();
+
+alert(dat.addDays(0))
 
 
-$(document).ready(function () {
-    Home.WireClickEvent();
-    Home.WireDragEvent();
-    
-    });
+function showAlert() {
+  alert("In Texas!")
+}
